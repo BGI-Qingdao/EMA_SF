8374a35a9a5626c6f18775a3ee6b656a8b72d700
Yanwei Qi <qiyanweii@icloud.com>
2023-02-28 15:21:30 +0800

    restore the ref and dict file

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,3 @@
 CURR_DIR=`pwd`
 mkdir -p $CURR_DIR/OUT_DIR
-/../pipeline/EMA_SF.sh -1 $CURR_DIR/chr8_400k.r1.fq.gz -2 $CURR_DIR/chr8_400k.r2.fq.gz -t 20 -r $CURR_DIR/chr8.fa -o $CURR_DIR/OUT_DIR
+/../pipeline/EMA_SF.sh -1 $CURR_DIR/chr8_400k.r1.fq.gz -2 $CURR_DIR/chr8_400k.r2.fq.gz -t 20 -r $CURR_DIR/../ref/chr8.fa -o $CURR_DIR/OUT_DIR
